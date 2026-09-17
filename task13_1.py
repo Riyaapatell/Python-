94470f80a7353e4951e732923ab07ffe1d4b13b0
Riyaapatell <riyapatel4962@gmail.com>
2026-09-17 11:26:13 +0530

    13th change

--- task13_1.py
+++ task13_1.py
@@ -49,3 +49,29 @@ while True:
         print("List exited""\n")
     else:
         print("Invalid choice""\n")
+
+# weather api
+import requests
+
+api_key = "8613f15eda2c9291eb0ba3e4d47a823d"
+
+city = input("Enter city: ")
+
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+
+response = requests.get(url)
+data = response.json()
+
+if response.status_code == 200:
+    print("City:", data["name"])
+    print("Temperature:", data["main"]["temp"], "°C")
+    print("Feels Like:", data["main"]["feels_like"], "°C")
+    print("Minimum Temperature:", data["main"]["temp_min"], "°C")
+    print("Maximum Temperature:", data["main"]["temp_max"], "°C")
+    print("Humidity:", data["main"]["humidity"], "%")
+    print("Weather:", data["weather"][0]["description"])
+    print("Wind Speed:", data["wind"]["speed"], "m/s")
+    print("Cloudiness:", data["clouds"]["all"], "%")
+    print("Visibility:", data["visibility"], "m")
+else:
+    print("Entered city DNE./Entered a valid city.")
